2e9c8a491ecd53ed965ef379a69ca2a6a65833e5
podo <cglee079@gmail.com>
2024-09-21 19:51:00 +0900

    feat : part1 - basic

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.14.0")
+    implementation("io.sentry:sentry-logback:7.14.0")
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/org/cglee079/hello/elkthread/controller/HealthController.kt
+++ src/main/kotlin/org/cglee079/hello/elkthread/controller/HealthController.kt
@@ -1,11 +1,15 @@
 package org.cglee079.hello.elkthread.controller
 
 import io.sentry.Sentry
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthController {
+
+    val logger = LoggerFactory.getLogger(HealthController::class.java)
+
 
     @GetMapping("/api/ping")
     fun ok(): String {
@@ -15,5 +19,26 @@ class HealthController {
     @GetMapping("/api/throw")
     fun throwException(): String {
         throw Exception("Throw Test Exception")
+    }
+
+    @GetMapping("/api/throw2")
+    fun throwException2(): String {
+        logger.error("Throw Test Exception2")
+        return "Throw Exception!"
+    }
+
+    @GetMapping("/api/throw3")
+    fun throwException3(): String {
+        Sentry.captureException(RuntimeException("Throw Test Exception3"))
+        return "Throw Exception!"
+    }
+
+    @GetMapping("/api/throw-with-try-catch")
+    fun throwExceptionWithTryCatch(txId: String): String {
+        try {
+            throw RuntimeException("Test Exception")
+        } catch (e: Exception) {
+            throw RuntimeException("$txId 결제건에서 예외가 발생하였습니다.")
+        }
     }
 }
